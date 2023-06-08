e22411af62c449115d04ddb3c8aeb8542f8415bb
Matěj Lajtkep <matej.lajtkep@gmail.com>
2023-06-08 22:54:45 +0200

    connection to MySql database

---
+++ LightGallery/Models/User.cs
@@ -0,0 +1,7 @@
+﻿namespace LightGallery.Models;
+
+public class User
+{
+    public Guid Id { get; set; }
+    public string UserName { get; set; }
+}
--- LightGallery/Program.cs
+++ LightGallery/Program.cs
@@ -1,11 +1,24 @@
+using System.Text.Json.Serialization;
+using LightGallery;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddControllers().AddJsonOptions(x =>
+{
+    x.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var connectionString = builder.Configuration.GetConnectionString("Default");
+builder.Services.AddDbContext<DefaultDatabaseContext>(o =>
+    o.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))
+);
+    
 
 var app = builder.Build();
 

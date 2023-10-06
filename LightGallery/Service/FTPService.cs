c0cad0cc28b2559c4aa1db97973ec6600cb30e43
Matěj Lajtkep <lajtkep@xdent.cz>
2023-10-06 22:00:21 +0200

    quicksave

--- LightGallery/Service/FTPService.cs
+++ LightGallery/Service/FTPService.cs
@@ -1,11 +1,13 @@
 using FluentFTP;
+using LightGallery.Models;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 namespace LightGallery.Service;
 
 public interface IFTPService
 {
-    public Task<Stream> GetFile(Stream s, string fileName);
+    public Task UploadFile(GalleryFile galleryFile, Stream fileStream);
+    public Task<Stream> GetFile(GalleryFile galleryFile);
 }
 
 public class FTPService : IFTPService
@@ -27,18 +29,25 @@ public class FTPService : IFTPService
         _host = _config["FTP:Host"];
     }
 
-
-    public async Task<Stream> GetFile(Stream fileStream, string fileName)
+    public string GetFileRemotePath(GalleryFile galleryFile)
+    {
+        return $"./{galleryFile.IdGallery}/{galleryFile.FolderIndex}/{galleryFile.Id}.{galleryFile.Extension}";
+    }
+    
+    public async Task UploadFile(GalleryFile galleryFile, Stream fileStream)
     {
         var client = new AsyncFtpClient(_host, _username, _password, 21);
         await client.AutoConnect();
 
-        var b = await client.GetWorkingDirectory();
-        var a = await client.GetFileSize("./testImage.jpg");
-        
-        var result = await client.DownloadStream(fileStream, "testImage.jpg");
-        
+        await client.UploadStream(fileStream, GetFileRemotePath(galleryFile), FtpRemoteExists.Overwrite, true);
+    }
 
-        return fileStream;
+
+    public async Task<Stream> GetFile(GalleryFile galleryFile)
+    {
+        var client = new AsyncFtpClient(_host, _username, _password, 21);
+        await client.AutoConnect();
+        
+        return await client.OpenRead(GetFileRemotePath(galleryFile));
     }
 }
--- LightGallery/Service/FileService.cs
+++ LightGallery/Service/FileService.cs
@@ -1,73 +1,51 @@
-﻿using AutoMapper;
-using LightGallery.Models;
-using LightGallery.Models.Results;
-using Microsoft.EntityFrameworkCore;
-
-namespace LightGallery.Service;
-
-public interface IFileService
-{
-    public Task<TemporaryFileResult> UploadFileToTemporaryStorage(Guid idUser, IFormFile file);
-    public Task<bool> MakeFilePermanent(string oldPath, GalleryFile galleryFile);
-    public Task<string> GetFileFolder(GalleryFile galleryFile);
-    public Task<string> GetFilePath(GalleryFile galleryFile);
-}
-
-public class FileService : IFileService
-{
-    private readonly IMapper _mapper;
-    private readonly IWebHostEnvironment _hostingEnvironment;
-    private readonly string _rootPath;
-
-    public FileService(IWebHostEnvironment context, IMapper mapper)
-    {
-        _hostingEnvironment = context;
-        _mapper = mapper;
-        _rootPath = _hostingEnvironment.ContentRootPath;
-    }
-    
-    public async Task<TemporaryFileResult> UploadFileToTemporaryStorage(Guid idUser, IFormFile file)
-    {
-        var directoryPath = Path.Combine(_rootPath, "Files", idUser.ToString(), "TempUpload");
-        
-        var extension = Path.GetExtension(file.FileName);
-        var filePath = Path.Combine(directoryPath, DateTime.UtcNow.Ticks + extension);
-
-        Directory.CreateDirectory(directoryPath);
-
-        await using (var fileStream = new FileStream(filePath, FileMode.Create))
-        {
-            await file.CopyToAsync(fileStream);
-        }
-
-        var fileInfo = new FileInfo(filePath);
-        
-        return new TemporaryFileResult()
-        {
-            Success = true,
-            Extension = extension,
-            Path = filePath,
-            FileSize = fileInfo.Length / 1024,
-            MimeType = file.ContentType
-        };
-    }
-
-    public async Task<bool> MakeFilePermanent(string oldPath, GalleryFile galleryFile)
-    {
-        var newPath = await GetFilePath(galleryFile);
-        Directory.CreateDirectory(await GetFileFolder(galleryFile));
-        File.Move(oldPath, newPath);
-        return true;
-    }
-
-    public Task<string> GetFileFolder(GalleryFile galleryFile)
-    {
-        var folderIndex = $"{galleryFile.FolderIndex:0000}";
-        return Task.FromResult(Path.Combine(_rootPath, "Files", galleryFile.IdOwner.ToString(), galleryFile.IdGallery.ToString(), $"Chunk_{folderIndex}"));
-    }
-    
-    public async Task<string> GetFilePath(GalleryFile galleryFile)
-    {
-        return Path.Combine(await GetFileFolder(galleryFile), galleryFile.Id + galleryFile.Extension);
-    }
-}
+﻿// using AutoMapper;
+// using LightGallery.Models;
+// using LightGallery.Models.Results;
+// using Microsoft.EntityFrameworkCore;
+//
+// namespace LightGallery.Service;
+//
+// public interface IFileService
+// {
+//     public Task<Stream> GetFile(Guid idGalleryFile);
+// }
+//
+// public class EndoraFileService : IFTPService
+// {
+//     private IConfiguration _config;
+//
+//     private readonly string _username;
+//     private readonly string _password;
+//     private readonly string _host;
+//     private readonly int _port;
+//     
+//
+//     public EndoraFileService(IConfiguration config)
+//     {
+//         _config = config;
+//         
+//         _username = _config["FTP:UserName"];
+//         _password= _config["FTP:Password"];
+//         _host = _config["FTP:Host"];
+//     }
+//
+//
+//     public async Task<Stream> GetFile(Guid idGalleryFile)
+//     {
+//         
+//         try
+//         {
+//             if (_ftpClient.FileExists(remoteFilePath))
+//             {
+//                 Stream ftpStream = _ftpClient.OpenRead(remoteFilePath);
+//                 return File(ftpStream, "application/octet-stream", filename);
+//             }
+//             else
+//             {
+//                 return NotFound("File not found on FTP server.");
+//             }
+//         }
+//
+//         return fileStream;
+//     }
+// }
